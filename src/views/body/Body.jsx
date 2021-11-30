75ee3fdcf3725720a35f0d59b9031b88d9235b1e
Dschahannam <uwunoozles@gmail.com>
2021-11-30 19:53:32 +0100

    Submit integration

--- src/views/body/Body.jsx
+++ src/views/body/Body.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './body.css'
 
-import { Button } from '../../components';
+import { Button, Input } from '../../components';
 
 const Body = () => {
     return (
         <div className="climeight__body">
             <div className="climeight__body-content">
-                <p>Werde jetzt Teil, des Klimaschutzes.</p>
-                <Button title="Anmelden" />
+                <div className="climeight__body-title-container">
+                    <p className="climeight__body-title">Werde jetzt Teil, des Klimaschutzes.</p>
+                    <p className="climeight__body-title-description">climeight. - die App, die das Fahrradfahren verändert.</p>
+                </div>
+
+                <p className="climeight__body-content-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                
+                <div className="climeight__input-content">
+                    <Input placeholder="Deine E-Mail Adresse:" />
+                    <Button title="Anmelden" />
+                </div>
             </div>
         </div>
     )
