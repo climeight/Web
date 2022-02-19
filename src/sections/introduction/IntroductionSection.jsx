409b4533aec830f8cab825c3b4a6045f8c793f47
Xalo Divo <70486444+Dschahannam@users.noreply.github.com>
2022-02-19 18:24:13 +0100

    Add Introduction Section & change header position

--- src/sections/introduction/IntroductionSection.jsx
+++ src/sections/introduction/IntroductionSection.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './css/IntroductionSectionStyle.css'
 
+import ImageEffectSmoke from '../../assets/effects/effect_smoke.png';
+
 const IntroductionSection = (props) => {
     return (
       <div className="climeight__introduction">
         <div className="climeight__container climeight__introduction__container">
+          <div className="climeight__introduction__effects">
+            <img className="climeight__introduction__effects-smoke" src={ImageEffectSmoke} alt="" />
+            <img className="climeight__introduction__effects-smoke" src={ImageEffectSmoke} alt="" />
+            <img className="climeight__introduction__effects-smoke" src={ImageEffectSmoke} alt="" />
+            <img className="climeight__introduction__effects-smoke" src={ImageEffectSmoke} alt="" />
+          </div>
+
+          <div className="climeight__introduction__text">
+            <p>Fahrrad fahren.</p>  
+            <p>Klima retten.</p>  
+            <p>Prämien verdienen.</p>  
+          </div>
         </div>
       </div>
     )
