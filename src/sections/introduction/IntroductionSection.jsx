020e65a04aef797b134200cab15cee1225b0136b
Dschahannam <uwunoozles@gmail.com>
2022-02-25 10:37:52 +0100

    Add scrolling frames in introduction section

--- src/sections/introduction/IntroductionSection.jsx
+++ src/sections/introduction/IntroductionSection.jsx
@@ -1,12 +1,63 @@
 import React from 'react'
+import $ from 'jquery';
+
 import './css/IntroductionSectionStyle.css'
 
 import ImageEffectSmoke from '../../assets/effects/effect_smoke.png';
 
 const IntroductionSection = (props) => {
+    React.useEffect(() => {
+      const handler = () => {
+        $('.climeight__introduction').each(function () {
+          var hold = $(this);
+      
+          var pos_scroll_frame_1 = ((hold.find('.climeight_introduction__scroll-frame-1').offset().top - $(window).scrollTop() - $(window).outerHeight()) / hold.find('.climeight_introduction__scroll-frame-1').outerHeight() * -1).toFixed(6);
+          var pos_scroll_frame_2 = ((hold.find('.climeight_introduction__scroll-frame-2').offset().top - $(window).scrollTop() - $(window).outerHeight()) / hold.find('.climeight_introduction__scroll-frame-2').outerHeight() * -1).toFixed(6);
+          var pos_scroll_frame_3 = ((hold.find('.climeight_introduction__scroll-frame-3').offset().top - $(window).scrollTop() - $(window).outerHeight()) / hold.find('.climeight_introduction__scroll-frame-3').outerHeight() * -1).toFixed(6);
+          
+          var temp_scroll_frame_1 = 0;
+          var temp_scroll_frame_2 = 0;
+          var temp_scroll_frame_3 = 0;
+      
+          if (pos_scroll_frame_1 < 0) {
+            temp_scroll_frame_1 = 0;
+          } else if (pos_scroll_frame_1 > 1) {
+            temp_scroll_frame_1 = 1;
+          } else {
+            temp_scroll_frame_1 = pos_scroll_frame_1;
+          }
+      
+          if (pos_scroll_frame_2 < 0) {
+            temp_scroll_frame_2 = 0;
+          } else if (pos_scroll_frame_2 > 1) {
+            temp_scroll_frame_2 = 1;
+          } else {
+            temp_scroll_frame_2 = pos_scroll_frame_2;
+          }
+      
+          if (pos_scroll_frame_3 < 0) {
+            temp_scroll_frame_3 = 0;
+          } else if (pos_scroll_frame_3 > 1) {
+            temp_scroll_frame_3 = 1;
+          } else {
+            temp_scroll_frame_3 = pos_scroll_frame_3;
+          }
+
+          hold.css({
+            '--scroll-frame-1': temp_scroll_frame_1,
+            '--scroll-frame-2': temp_scroll_frame_2,
+            '--scroll-frame-3': temp_scroll_frame_3
+          });
+        });
+      };
+
+      window.addEventListener("scroll", handler);
+      return () => window.removeEventListener("scroll", handler);
+    }, []);
+
     return (
       <div className="climeight__introduction">
-        <div className="climeight__container climeight__introduction__container">
+        <div className="climeight__introduction__container">
           <div className="climeight__introduction__effects">
             <img className="climeight__introduction__effects-smoke" src={ImageEffectSmoke} alt="" />
             <img className="climeight__introduction__effects-smoke" src={ImageEffectSmoke} alt="" />
@@ -20,6 +71,13 @@ const IntroductionSection = (props) => {
             <p>Prämien verdienen.</p>  
           </div>
         </div>
+
+        <div class="climeight_introduction__scroll-frame climeight_introduction__scroll-frame-1"></div>
+				<div class="climeight_introduction__scroll-frame climeight_introduction__scroll-frame-2"></div>
+				<div class="climeight_introduction__scroll-frame climeight_introduction__scroll-frame-3"></div>
+
+        { /* To smoothize scrolling frame at the end */ }
+				<div class="climeight_introduction__scroll-frame"></div>
       </div>
     )
 }
